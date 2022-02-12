601e58fb95c639c158ec3e50bc813efbfc4885a4
Matt L <primemuon@gmail.com>
2022-02-12 19:23:55 +0000

    A working version of the login page

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 from flask import Flask
+from flask import request, redirect
 
 from backend import Account
 
 def main():
+
+    ADMIN_USER_NAME = "admin1"
+    USER_USER_NAME = "user1"
+
     app = Flask(__name__)
     account = Account()
 
     # login page
-    @app.route("/")
+    @app.route("/", methods=['GET', 'POST'])
     def returnLoginPage():
-        return open("./login_page.html") \
-                .read() \
-                .format(var1="login page")
+        userName = request.form.get("username")
+
+        print("[debug] user name inputted: {}".format(userName))
+
+        if userName == ADMIN_USER_NAME:
+            return redirect("http://127.0.0.1:5000/admin", code=302)
+        elif userName == USER_USER_NAME:
+            return redirect("http://127.0.0.1:5000/user", code=302)
+
+        return open("./static/home.html").read()
 
     # admin page
     @app.route("/admin")
     def returnAdminPage():
-        return open("./admin_page.html") \
+        return open("./static/admin_page.html") \
                 .read() \
                 .format(var1="admin page")
 
     # user page
     @app.route("/user")
     def returnUserPage():
-        return open("./user_page.html") \
+        return open("./static/user_page.html") \
                 .read() \
                 .format(var1="user page")
 
